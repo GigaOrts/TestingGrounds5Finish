2a4dc9cbf908087269d6f6d59d535c7d965811a1
GigaOrts <ortsortskhoev95@gmail.com>
2022-11-07 10:22:16 +0300

    Добавляю Меню игры

--- Assets/Course Library/Scripts/GameController.cs
+++ Assets/Course Library/Scripts/GameController.cs
@@ -2,35 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public List<GameObject> targets;
+
+    public Button restartButton;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
 
     private int score;
     private float spawnRate = 1f;
 
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
     }
 
-    private IEnumerator SpawnTarget()
+    public void GameOver()
     {
-        while(true)
-        {
-            yield return new WaitForSeconds(spawnRate);
-            int index = Random.Range(0, targets.Capacity);
-
-            Instantiate(targets[index]);
-        }
+        isGameActive = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
     }
 
     public void UpdateScore(int scoreToUpdate)
     {
         score += scoreToUpdate;
         scoreText.text = $"Score: {score}";
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private IEnumerator SpawnTarget()
+    {
+        while(isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Capacity);
+
+            Instantiate(targets[index]);
+        }
     }
 }
